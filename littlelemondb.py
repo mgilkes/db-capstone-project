bbc3bcda829417b0287091db7e28561dbb558455
Michael Gilkes <jaido7@yahoo.com>
2025-07-27 12:37:31 -0400

    added comments to the py file

--- littlelemondb.py
+++ littlelemondb.py
@@ -1,19 +1,23 @@
+# import the mysql python connector
 import mysql.connector as connector
 
+# connect to the database
 connection = connector.connect(
     user="capstone",
     password="db-Capstone-Course8"
 )
 
+# get the cursor and use the database
 cursor = connection.cursor()
 cursor.execute("USE LittleLemonDB")
 
+# list the database tables
 show_tables_query = "SHOW tables"
 cursor.execute(show_tables_query)
-
 results = cursor.fetchall()
 print(results)
 
+# Execute the query for Task 3 and display the results
 query1 = """SELECT 
     CONCAT(c.FirstName, ' ', c.LastName) AS FullName, 
     c.Phone, 
